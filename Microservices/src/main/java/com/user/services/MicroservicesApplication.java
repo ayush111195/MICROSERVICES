34f6088abc01f90e1727ac3bd87bb4b2b7d84611
Ayush gupta <ayush111195>
2023-03-14 23:28:09 +0530

    connected with eureka

--- Microservices/src/main/java/com/user/services/MicroservicesApplication.java
+++ Microservices/src/main/java/com/user/services/MicroservicesApplication.java
@@ -2,12 +2,10 @@ package com.user.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.user.services.entites.User;
-import com.user.services.repositories.UserRepository;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
--- Microservices/src/main/java/com/user/services/services/impl/UserServiceImpl.java
+++ Microservices/src/main/java/com/user/services/services/impl/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.user.services.services.impl;
 
 import java.util.List;
-import java.util.Optional;
+
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
