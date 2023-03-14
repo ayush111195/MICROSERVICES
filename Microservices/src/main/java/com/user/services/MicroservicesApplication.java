d587e9d1429633e95870adfa0a21bdc34d5fcdc3
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-03-14 22:40:14 +0530

    Add files via upload

--- Microservices/src/main/java/com/user/services/MicroservicesApplication.java
+++ Microservices/src/main/java/com/user/services/MicroservicesApplication.java
@@ -2,19 +2,12 @@ package com.user.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.user.services.entites.User;
-import com.user.services.repositories.UserRepository;
 
 @SpringBootApplication
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
-			SpringApplication.run(MicroservicesApplication.class, args);
-	
+		SpringApplication.run(MicroservicesApplication.class, args);
 	}
-	
-	
 
 }
