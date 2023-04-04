bbe08844bd15158cd68bc91cb03583e51b7b8b33
Ayush gupta <ayush111195>
2023-04-05 01:44:29 +0530

    CLIENT-SECURITY

--- Microservices/src/main/java/com/user/services/config/MyConfiguration.java
+++ Microservices/src/main/java/com/user/services/config/MyConfiguration.java
@@ -1,17 +1,61 @@
 package com.user.services.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
+
+import com.user.services.config.interceptor.RestTemplateInterceptor;
 
 @Configuration
 public class MyConfiguration {
+	
+	@Autowired
+	private ClientRegistrationRepository clientRegistrationRepository;
+	
+	@Autowired
+	private OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository;
 
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
 		
+		RestTemplate restTemplate = new RestTemplate();
+		List<ClientHttpRequestInterceptor> interceptors=new ArrayList<>();
+		
+		interceptors.add( new RestTemplateInterceptor(manager(clientRegistrationRepository, auth2AuthorizedClientRepository )));
+		
+		restTemplate.setInterceptors(interceptors);
+		
+		return restTemplate();
+		
+	}
+	
+	//DECLARE THE BEAN OF OAuth2AuthorizedClient Manager(FeignClientInterrceptor)
+	
+	@Bean
+	public OAuth2AuthorizedClientManager manager(
+			ClientRegistrationRepository clientRegistrationRepository,
+			OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository
+			) {
+		
+		OAuth2AuthorizedClientProvider provider= OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
+		
+		DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager	= new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, auth2AuthorizedClientRepository);
+		
+		defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
+		
+		return defaultOAuth2AuthorizedClientManager;
 	}
 }
---
+++ Microservices/src/main/java/com/user/services/config/interceptor/FeignClientInterceptor.java
@@ -0,0 +1,30 @@
+package com.user.services.config.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Component;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+@Configuration
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+	private OAuth2AuthorizedClientManager manager;  // IS CLASS KI HELP SE HUM TOKEN NIKAL SKTE H --->>>
+	
+	@Override
+	public void apply(RequestTemplate template) {
+		
+String token=manager.authorize(OAuth2AuthorizeRequest.withClientRegistrationId("my-internal-client")
+		.principal("internal")
+		.build())
+        .getAccessToken()
+        .getTokenValue();
+		
+		template.header("Authorization", "Bearer"+token);
+		
+	}
+
+}
